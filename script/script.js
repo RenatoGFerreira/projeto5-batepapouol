9e1bbed5d89ddcc06608e9f670811fc17b18af6d
Renato Ferreira <renatoyferreira@gmail.com>
2022-08-25 11:51:43 -0300

    html, css estilizados

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+
+function showMenu(){
+    const item = document.querySelector('.menu-site')
+    item.classList.toggle('menu-site-aparecer')
+}
+
+function hideMenu(){
+    const item = document.querySelector('.menu-site')
+    item.classList.toggle('menu-site-aparecer')
+}
